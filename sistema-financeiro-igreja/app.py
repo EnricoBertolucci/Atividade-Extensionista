240eb01c04950667d09548b5a1bfa0e3d26e09c2
Enrico Bertolucci <negocioonlinebertolucci@gmail.com>
2026-09-23 11:07:20 -0300

    feat: melhorias de usabilidade (menu responsivo, tabelas responsivas, tela de ajuda)

--- sistema-financeiro-igreja/app.py
+++ sistema-financeiro-igreja/app.py
@@ -68,5 +68,11 @@ def index():
     )
 
 
+@app.route("/ajuda")
+@login_required
+def ajuda():
+    return render_template("ajuda.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
